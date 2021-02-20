3fb29c1fc2304db6f998963df119a4e8c40f97c2
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-02-20 10:10:31 +0800

    Add files via upload

---
+++ Automated_LightV1.ino
@@ -0,0 +1,42 @@
+#include <IRremote.h>
+#include <Servo.h>
+
+const int RECV_PIN = 3;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+unsigned long key_value = 0;
+int yes = 5;
+
+Servo servo1;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+  servo1.attach(yes);
+}
+
+void loop(){
+  if (irrecv.decode(&results)){
+ 
+        if (results.value == 0XFFFFFFFF)
+          results.value = key_value;
+
+        switch(results.value){
+          case 0xFFA25D:
+          Serial.println("OFF");
+          servo1.write(70);
+          delay(2000);
+          servo1.write(90);
+          break;
+          case 0xFF629D:
+          Serial.println("ON");
+          servo1.write(110);
+          delay(2000);
+          servo1.write(90);
+          break;
+        }
+        key_value = results.value;
+        irrecv.resume(); 
+  }
+}
